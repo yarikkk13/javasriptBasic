0dbab259cab8316d3f676c8871a67aed4d6b7499
yarikkk13 <yaroslav.mahas@gmail.com>
2021-03-24 19:52:07 +0200

    Google "String to number JS

--- lesson01/lesson01.js
+++ lesson01/lesson01.js
@@ -1,70 +1,126 @@
 // let a = 'hello world';
 // console.log(a);
 //  Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
-let a = 'hello';
-let company = 'owu';
-let word = 'com';
-let country = 'ua';
-let number = 1;
-let ten = 10;
-let negNumber = -999;
-let sequence = 123;
-let pi = 3.14;
-let x = 2.7;
-let y = 16;
-let Yarik = true;
-let Igor = false;
- // Вивести кожну змінну за допомогою: console.log , alert, document.write
-console.log(a);
-console.log(company);
-console.log(word);
-console.log(country);
-console.log(number);
-console.log(ten);
-console.log(negNumber);
-console.log(sequence);
-console.log(pi);
-console.log(x);
-console.log(y);
-console.log(Yarik);
-console.log(Igor);
-alert(a);
-alert(company);
-alert(word);
-alert(country);
-alert(number);
-alert(ten);
-alert(negNumber);
-alert(sequence);
-alert(pi);
-alert(x);
-alert(y);
-alert(Yarik);
-alert(Igor);
-document.write(a);
-document.write(company);
-document.write(word);
-document.write(country);
-document.write(number);
-document.write(ten);
-document.write(negNumber);
-document.write(sequence);
-document.write(pi);
-document.write(x);
-document.write(y);
-document.write(Yarik);
-document.write(Igor);
-// Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
-a = 'bye';
-company = 'wiki';
-word = 'random';
-country = 'us';
-number = 10;
-ten = 100;
-negNumber = -99;
-sequence = 1234;
-pi = 3.1415;
-x = 2.75;
-y = 4;
-Yarik = false;
-Igor = true;
+// let a = 'hello';
+// let company = 'owu';
+// let word = 'com';
+// let country = 'ua';
+// let number = 1;
+// let ten = 10;
+// let negNumber = -999;
+// let sequence = 123;
+// let pi = 3.14;
+// let x = 2.7;
+// let y = 16;
+// let Yarik = true;
+// let Igor = false;
+//  // Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(a);
+// console.log(company);
+// console.log(word);
+// console.log(country);
+// console.log(number);
+// console.log(ten);
+// console.log(negNumber);
+// console.log(sequence);
+// console.log(pi);
+// console.log(x);
+// console.log(y);
+// console.log(Yarik);
+// console.log(Igor);
+// alert(a);
+// alert(company);
+// alert(word);
+// alert(country);
+// alert(number);
+// alert(ten);
+// alert(negNumber);
+// alert(sequence);
+// alert(pi);
+// alert(x);
+// alert(y);
+// alert(Yarik);
+// alert(Igor);
+// document.write(a);
+// document.write(company);
+// document.write(word);
+// document.write(country);
+// document.write(number);
+// document.write(ten);
+// document.write(negNumber);
+// document.write(sequence);
+// document.write(pi);
+// document.write(x);
+// document.write(y);
+// document.write(Yarik);
+// document.write(Igor);
+// // Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+// a = 'bye';
+// company = 'wiki';
+// word = 'random';
+// country = 'us';
+// number = 10;
+// ten = 100;
+// negNumber = -99;
+// sequence = 1234;
+// pi = 3.1415;
+// x = 2.75;
+// y = 4;
+// Yarik = false;
+// Igor = true;
+// // Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(a);
+// console.log(company);
+// console.log(word);
+// console.log(country);
+// console.log(number);
+// console.log(ten);
+// console.log(negNumber);
+// console.log(sequence);
+// console.log(pi);
+// console.log(x);
+// console.log(y);
+// console.log(Yarik);
+// console.log(Igor);
+// alert(a);
+// alert(company);
+// alert(word);
+// alert(country);
+// alert(number);
+// alert(ten);
+// alert(negNumber);
+// alert(sequence);
+// alert(pi);
+// alert(x);
+// alert(y);
+// alert(Yarik);
+// alert(Igor);
+// document.write(a);
+// document.write(company);
+// document.write(word);
+// document.write(country);
+// document.write(number);
+// document.write(ten);
+// document.write(negNumber);
+// document.write(sequence);
+// document.write(pi);
+// document.write(x);
+// document.write(y);
+// document.write(Yarik);
+// document.write(Igor);
+// // За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// let name = prompt('Яке твоє імя?');
+// let secondName = prompt('Як тебе по батькові?');
+// let age = prompt('скільки тобі років?');
+// // Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// console.log('Вітаю'+ ' '+ name + ' ' + secondName + '. ' + 'Тобі ' + age + ' років.');
+// // Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація).
+// let obyednanaZminna = name + secondName + age; // я правильно зрозумів умову задачі?
+// За допомогою prompt() отримати 3 числа. Привести їх до числа (Google "String to number JS"). Вивести їх в консоль.
+let num1 = +prompt('перше число?')
+let num2 = +prompt('друге число?')
+let num3 = +prompt('третє число?')
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(typeof num2)
