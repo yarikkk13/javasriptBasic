59ec5214a3b65bfba14e72a1e0fe725f742e805c
yarikkk13 <yaroslav.mahas@gmail.com>
2021-03-31 14:49:08 +0300

    beginning

---
+++ lesson03/js/lesson03.1.js
@@ -0,0 +1,76 @@
+// - Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому.
+//
+// 1) Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або
+// document.getElementsByTagName :
+//     a) отримує текст з параграфа з id "content"
+//     b) отримує текст з блоку з id "rules"
+//     c) замініть текст параграфа з id 'content' на будь-який інший
+//     d) замініть текст параграфа з id 'rules' на будь-який інший
+//     e) змініть кожному елементу колір фону на червоний
+//     f) змініть кожному елементу колір тексту на синій
+//     g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+//     h) отримати всі елементи з класом fc_rules
+//     i) поміняти колір тексту у всіх елементів fc_rules на червоний
+//
+//
+// 2) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Вставити цей блок на сторінку
+// 3) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Блок з адресою зробити окремим блоком, з блоками для кожної властивості
+//
+// let users = [{
+//   name: 'vasya',
+//   age: 31,
+//   status: false,
+//   address: { city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1 }
+// }, {
+//   name: 'petya',
+//   age: 30,
+//   status: true,
+//   address: { city: 'New York', country: 'USA', street: 'East str', houseNumber: 21 }
+// }, {
+//   name: 'kolya',
+//   age: 29,
+//   status: true,
+//   address: { city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78 }
+// }, {
+//   name: 'olya',
+//   age: 28,
+//   status: false,
+//   address: { city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56 }
+// }, {
+//   name: 'max',
+//   age: 30,
+//   status: true,
+//   address: { city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39 }
+// }, {
+//   name: 'anya',
+//   age: 31,
+//   status: false,
+//   address: { city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5 }
+// }, {
+//   name: 'oleg',
+//   age: 28,
+//   status: false,
+//   address: { city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90 }
+// }, {
+//   name: 'andrey',
+//   age: 29,
+//   status: true,
+//   address: { city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33 }
+// }, {
+//   name: 'masha',
+//   age: 30,
+//   status: true,
+//   address: { city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1 }
+// }, {
+//   name: 'olya',
+//   age: 31,
+//   status: false,
+//   address: { city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4 }
+// }, {
+//   name: 'max',
+//   age: 31,
+//   status: true,
+//   address: { city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45 }
+// }];
