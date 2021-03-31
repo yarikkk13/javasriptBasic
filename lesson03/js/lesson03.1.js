aca35cec20f1a18b8a64e4addd7d3f220139df2c
yarikkk13 <yaroslav.mahas@gmail.com>
2021-03-31 15:15:17 +0300

    beginning+2

--- lesson03/js/lesson03.1.js
+++ lesson03/js/lesson03.1.js
@@ -3,14 +3,32 @@
 // 1) Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або
 // document.getElementsByTagName :
 //     a) отримує текст з параграфа з id "content"
+console.log(document.getElementById('content'))
+
 //     b) отримує текст з блоку з id "rules"
+console.log(document.getElementById('rules'))
+
 //     c) замініть текст параграфа з id 'content' на будь-який інший
+
+
 //     d) замініть текст параграфа з id 'rules' на будь-який інший
+
+
 //     e) змініть кожному елементу колір фону на червоний
+
+
 //     f) змініть кожному елементу колір тексту на синій
+
+
 //     g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+
 //     h) отримати всі елементи з класом fc_rules
+
+
 //     i) поміняти колір тексту у всіх елементів fc_rules на червоний
+
+
 //
 //
 // 2) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
