a0b4fccd4f00678c91935c618f19eb49bfe59e56
yarikkk13 <yaroslav.mahas@gmail.com>
2021-04-10 23:38:47 +0300

    lesson06.3 start

--- lesson06/js/lesson06.3.js
+++ lesson06/js/lesson06.3.js
@@ -4,13 +4,70 @@
 //
 // Створити обєкт автомобіля з полями:
 // Марка автомобля, потужність двигуна, власник, ціна, рік випуску.
+class Car {
+    constructor(producer, powerEngine, owner, price, year) {
+        this.producer = producer;
+        this.powerEngine = powerEngine;
+        this.owner = owner;
+        this.price = price;
+        this.year = year
+    }
+    tuning () {
+        this.powerEngine = this.powerEngine*1.1
+    }
+    changeDriver(newDriver){
+        this.owner = newDriver
+    }
+
+}
+
 // Власник автомобіля теж має бути обєкт, у якого є поля
+class Owner {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience
+    }
+}
+
 // Імя, вік, стаж водіння.
+let vasya = new Owner('vasya', 23,2);
+let petya = new Owner('petya', 22,1);
+let taras = new Owner('taras', 24,4);
+let olya = new Owner('olya', 28,8);
+let king = new Owner('king', 20,10);
+let vika = new Owner('vika', 29,7);
+let motrya = new Owner('motrya', 18,6);
+let vova = new Owner('vova', 43,4);
+let luigi = new Owner('luigi', 50,28);
+let winchester = new Owner('winchester', 28,11);
 // Створити не менше 7 та не більше 20 машинок.
+let porshe = new Car('porshe', 502, vasya, 12000, 2012);
+let bmw = new Car('bmw', 300, petya, 10000, 2010);
+let hyundai = new Car('hyundai', 350, taras, 8000, 2006);
+let honda = new Car('honda', 460, olya, 11000, 2011);
+let zapor = new Car('zaz',999 , king, 99999, 2021);
+let kia = new Car('kia', 230, vika, 2000, 1995);
+let bogdan = new Car('bogdan', 1000, motrya, 9999, 2021);
+let kalina = new Car('lada', 100, vova, 900, 2009);
+let ferrari = new Car('ferrari', 634, luigi, 10000, 2002);
+let impalka = new Car('chevrolet', 425, winchester, 23000, 1967);
 // Зробили половину автопарку ремонт мотору, що збільшить потужність автомобілів на 10% (переприсвоєння змінної
 // потужності).
+porshe.tuning();
+honda.tuning();
+zapor.tuning();
+bogdan.tuning();
+ferrari.tuning();
 // На відремонтовані автомобілі найняти нових водіїв (переприсвоїти змінну водій).
+porshe.changeDriver(petya);
+honda.changeDriver(motrya);
+zapor.changeDriver(taras);
+bogdan.changeDriver(vika);
+ferrari.changeDriver(winchester)
 // Для початку вкладіть всі наші створені автомобілі в масив cars.
+let cars = [porshe, bmw, hyundai, honda, zapor,kia, bogdan,kalina,ferrari,impalka];
+console.log(cars);
 // Далі необхідно рати кожну другу машинку (цикл з кроком в 2), та робити їй підвищення потужності двигуна на
 // 10% та ціну на 5%
 // Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік
