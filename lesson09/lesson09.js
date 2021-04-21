14a296ea7e4c09ff5108fd5fe2ab2abc48a12d00
yarikkk13 <yaroslav.mahas@gmail.com>
2021-04-21 12:38:27 +0300

    09 promise is done

--- lesson09/lesson09.js
+++ lesson09/lesson09.js
@@ -182,3 +182,123 @@ console.log('hello world')
 // })
 
 
+// // 2. PROMISE
+//
+// function wakeUp(wokeUp) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (wokeUp) {
+//                 resolve(8);
+//             } else {
+//                 reject('u are late')
+//             }
+//         }, 8000)
+//     })
+// }
+//
+// function brushTeeth(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (time === 8) {
+//                 resolve(8.15);
+//             } else {
+//                 reject('ur teeth are not brushed')
+//             }
+//         }, 500)
+//     })
+// }
+//
+// function eating(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (time === 8.15) {
+//                 resolve(8.35);
+//             } else {
+//                 reject('u are hungry')
+//             }
+//         }, 700)
+//     })
+// }
+//
+// function driving(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (time === 8.35) {
+//                 resolve(9.00);
+//             } else {
+//                 reject('u are late on job')
+//             }
+//         }, 750)
+//     })
+// }
+//
+// function jobIsDone(time) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (time === 9) {
+//                 resolve(300);
+//             } else {
+//                 reject('u re job is not done')
+//             }
+//         }, 6000)
+//     })
+// }
+//
+// function shopping(money) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (money >= 300) {
+//                 resolve(money - 30);
+//             } else {
+//                 reject('not enough money')
+//             }
+//         }, 300)
+//     })
+// }
+//
+// function driveHome(money) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (money > 50) {
+//                 resolve(19.00);
+//             } else {
+//                 reject('u cant get home')
+//             }
+//         }, 850)
+//     })
+// }
+//
+// wakeUp(true)
+//     .then(value => {
+//         console.log(value)
+//         return brushTeeth(value)
+//     })
+//     .then(value => {
+//         console.log(value)
+//         return eating(value)
+//     })
+//     .then(value => {
+//         console.log(value)
+//         return driving(value)
+//     })
+//     .then(value => {
+//         console.log(value)
+//         return jobIsDone(value)
+//     })
+//     .then(value => {
+//         console.log(value)
+//         return shopping(value)
+//     })
+//     .then(value => {
+//         console.log(value)
+//         return driveHome(value)
+//     })
+//     .then(value => {
+//         console.log(`you're at home at ${value} pm`)
+//     })
+//     .catch(reason => {
+//         console.warn(reason)
+//     })
+
+
+// 3. async await
